ae4444cf31752342a47343e146060a931d21979a
pouria <pouria.farahani.developer@gmail.com>
2026-02-23 09:55:13 +0330

    add i18n local

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import {useTr} from "@/lib/i18n/config";
+
 export default function Home() {
+    const [t] = useTr();
+
     return (
         <div>
-            <h1>Hi , This Is Core NeoBank</h1>
+            <h1>{t('title')}</h1>
         </div>
     )
 }
