75480f37a841bbaef407d9c075e4c3a803424f51
pouria <pouria.farahani.developer@gmail.com>
2026-02-24 08:24:02 +0330

    add shadcn

--- components/pages/bill/counter.tsx
+++ components/pages/bill/counter.tsx
@@ -1,5 +1,7 @@
 "use client"
 import useStore from "@/lib/store/src/useCounterStore";
+import { Button } from "@/components/ui/button";
+
 
 function Counter() {
     const { count, inc } = useStore()
@@ -7,8 +9,12 @@ function Counter() {
         <div>
             <span>{count}</span>
             <button onClick={inc}>one up</button>
+            <div className="flex items-center justify-center min-h-screen bg-gray-100">
+                <Button variant='destructive'>Shadcn Works!</Button>
+            </div>
         </div>
     )
 }
 
 export default Counter;
+
